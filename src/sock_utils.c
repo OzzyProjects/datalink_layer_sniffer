975471aeb1a43a4d767108f8886e62aaa414a3c8
ARMANGAU Etienne <81378159+OzzyProjects@users.noreply.github.com>
2021-10-02 17:49:35 +0200

    Update sock_utils.c

--- src/sock_utils.c
+++ src/sock_utils.c
@@ -1,4 +1,4 @@
-#include "sock_utils.h"
+##include "sock_utils.h"
 #include "parsing.h"
 
 /************************************* IN PROGRESS *************************************/
@@ -21,7 +21,7 @@ uint16_t in_cksum(uint16_t *addr, int len){
 
     // If an odd byte is left
     if (nleft == 1) {
-        *(unsigned char *) (&answer) = *(unsigned char *) w;
+        *(unsigned char *) (&answer) = *(unsigned char *)w;
         sum += answer;
     }
 
@@ -43,12 +43,14 @@ void process_frame(unsigned char* buffer, int size){
 
         case ETHERTYPE_ARP:
             printf("\nARP frame there!\n");
-            print_arp_packet(buffer);
+            print_ethernet_header(buffer, size);
+            print_arp_header(buffer);
             break;
 
         case ETHERTYPE_IEEE1905_1:
             printf("\nETHERTYPE_IEEE1905_1 frame there!\n");
             print_ethernet_header(buffer , size);
+            print_ieee_1905_header(buffer, size);
             break;
 
         case ETHERTYPE_HOMEPLUG:
@@ -100,7 +102,7 @@ void process_ip_packet(unsigned char* buffer, int size){
             break;
         
         case 2:  //IGMP Protocol
-            print_igmp_packet(buffer, size);
+            print_igmp_header(buffer, size);
             break;
         
         case 6:  //TCP Protocol
@@ -112,7 +114,6 @@ void process_ip_packet(unsigned char* buffer, int size){
             break;
         
         default: //Some Other Protocol like ARP etc.
-
             printf("\nUnknown IP Packet there : %x\n", iph->protocol);
             print_data(buffer, size);
     }
@@ -128,16 +129,16 @@ void print_ethernet_header(unsigned char* buffer, int size){
     printf("   |-Protocol            : %x \n", ntohs(eth->h_proto));
 }
 
-void print_arp_packet(unsigned char* buffer){
+void print_arp_header(unsigned char* buffer){
 
     arp_header *arphdr = (arp_header*)(buffer + ETH2_HEADER_LEN);
 
     printf("\nARP Header\n\n");
-    printf("   |-Hardware Type      : %u\n", ntohs(arphdr->hardware_type));
-    printf("   |-Protocol Type      : %u\n", ntohs(arphdr->protocol_type));
+    printf("   |-Hardware Type      : %x\n", ntohs(arphdr->hardware_type));
+    printf("   |-Protocol Type      : %x\n", ntohs(arphdr->protocol_type));
     
-    printf("   |-Opcode             : %u\t", ntohs(arphdr->opcode));
-    parse_arp_opcode_field(arphdr->opcode);
+    printf("   |-Opcode             : %x\t", ntohs(arphdr->opcode));
+    parse_arp_opcode_field(ntohs(arphdr->opcode));
 
     printf("\n   |-Source MAC Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",arphdr->sender_mac[0],arphdr->sender_mac[1],arphdr->sender_mac[2],arphdr->sender_mac[3],arphdr->sender_mac[4],arphdr->sender_mac[5]);
     printf("   |-Dest MAC Address   : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",arphdr->target_mac[0],arphdr->target_mac[1],arphdr->target_mac[2],arphdr->target_mac[3],arphdr->target_mac[4],arphdr->target_mac[5]);
@@ -151,13 +152,13 @@ void print_homeplug_av_header(unsigned char* buffer){
     homeplug_av_header* home_av_hdr = (homeplug_av_header*)(buffer + ETH2_HEADER_LEN);
 
     printf("\nHomeplug AV Header\n");
-    printf("   |-Protocol  : %u", home_av_hdr->protocol);
-    printf("   |-Type      : %u\t", htons(home_av_hdr->type));
+    printf("   |-Protocol  : %x\n", home_av_hdr->protocol);
+    printf("   |-Type      : %x\t\n", htons(home_av_hdr->type));
         if (htons(home_av_hdr->type) == HOMEPLUG_AV_REQ_BRIDGE)
         printf("\t(Get Bridge Information Request)\n");
     else
         printf("(Unknown Type)\n");
-    printf("   |-Frag     : %u\n", home_av_hdr->frag);
+    printf("   |-Frag     : %x\n", home_av_hdr->frag);
 
 }
 
@@ -166,33 +167,11 @@ void print_homeplug_header(unsigned char* buffer){
     homeplug_header* home_hdr = (homeplug_header*)(buffer + ETH2_HEADER_LEN);
 
     printf("\nHomeplug Header\n\n");
-    printf("   |-Control Field    : %u\n", home_hdr->ctrl_field);
-    printf("   |-MAC Entry        : %u\n", home_hdr->mac_entry);
-    printf("   |-Entry Length     : %u\n", home_hdr->entry_length);
+    printf("   |-Control Field    : %x\n", home_hdr->ctrl_field);
+    printf("   |-MAC Entry        : %x\n", home_hdr->mac_entry);
+    printf("   |-Entry Length     : %x\n", home_hdr->entry_length);
     printf("   |-Vendor Specific  : %02x%02X%02X\n", home_hdr->spe_vendor[0], home_hdr->spe_vendor[1],home_hdr->spe_vendor[2]);
 }
-
-void print_lltd_header(unsigned char* buffer){
-
-    lltd_header* lltd_hdr = (lltd_header*)(buffer + ETH2_HEADER_LEN);
-
-    printf("\nLLTD Header\n");
-    printf("   |-Version               : %u\n", lltd_hdr->version);
-    
-    printf("   |-Service Type          : %u\t", lltd_hdr->service_type);
-    parse_lltd_service_type_field(lltd_hdr->service_type);
-
-    printf("   |-Reserved              : %u\n", lltd_hdr->reserved);
-
-    printf("   |-Function              : %u\t", lltd_hdr->function);
-    parse_lltd_function_field(lltd_hdr->function);
-
-    printf("   |-Real Dest MAC Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",lltd_hdr->real_dest[0],lltd_hdr->real_dest[1],
-        lltd_hdr->real_dest[2],lltd_hdr->real_dest[3],lltd_hdr->real_dest[4],lltd_hdr->real_dest[5]);
-    printf("   |-Real Src MAC Address  : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",lltd_hdr->real_src[0],lltd_hdr->real_src[1],
-        lltd_hdr->real_src[2],lltd_hdr->real_src[3],lltd_hdr->real_src[4],lltd_hdr->real_src[5]);
-}
-
 
 void print_ieee_1905_header(unsigned char* buffer, int size){
 
@@ -206,7 +185,39 @@ void print_ieee_1905_header(unsigned char* buffer, int size){
     /* TODO : parse TLV */  
 }
 
-void print_igmp_packet(unsigned char* buffer, int size){
+void print_lltd_header(unsigned char* buffer){
+
+    lltd_header* lltd_hdr = (lltd_header*)(buffer + ETH2_HEADER_LEN);
+
+    printf("\nLLTD Header\n");
+    printf("   |-Version               : %x\n", lltd_hdr->version);
+    printf("   |-Service Type          : %x\t", lltd_hdr->service_type);
+    parse_lltd_service_type_field(lltd_hdr->service_type);
+    printf("   |-Reserved              : %x\n", lltd_hdr->reserved);
+    printf("   |-Function              : %x\t", lltd_hdr->function);
+    parse_lltd_function_field(lltd_hdr->function);
+    printf("   |-Real Dest MAC Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",lltd_hdr->real_dest[0],lltd_hdr->real_dest[1],
+        lltd_hdr->real_dest[2],lltd_hdr->real_dest[3],lltd_hdr->real_dest[4],lltd_hdr->real_dest[5]);
+    printf("   |-Real Src MAC Address  : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",lltd_hdr->real_src[0],lltd_hdr->real_src[1],
+        lltd_hdr->real_src[2],lltd_hdr->real_src[3],lltd_hdr->real_src[4],lltd_hdr->real_src[5]);
+}
+
+void print_pn_dcp_header(unsigned char* buffer){
+
+    pndcp_header* pndcp_hdr = (pndcp_header*)(buffer + ETH2_HEADER_LEN);
+
+    printf("\nPN-DCP Header\n");
+    printf("   |-Service ID            : %x\t", pndcp_hdr->serv_id);
+    parse_pndcp_service_id_field(pndcp_hdr->serv_id);
+    printf("   |-Service Type          : %x\t", pndcp_hdr->serv_type);
+    parse_pndcp_service_type_field(pndcp_hdr->serv_type);
+    printf("   |-Xid                   : %x\n", ntohl(pndcp_hdr->xid));
+    printf("   |-Option                : %x\t", pndcp_hdr->option);
+    parse_pndcp_option_field(pndcp_hdr->option);
+    printf("   |-Suboption             : %x\n", pndcp_hdr->sub_option);
+}
+
+void print_igmp_header(unsigned char* buffer, int size){
 
     unsigned short iphdrlen;
     struct iphdr *iph = (struct iphdr *)(buffer  + ETH2_HEADER_LEN);
@@ -220,14 +231,14 @@ void print_igmp_packet(unsigned char* buffer, int size){
 
     printf("\nIGMP Header\n");
 
-    printf("   |-Type      : %u\t", ighdr->igmp_type);
+    printf("   |-Type      : %x\t\n", ighdr->igmp_type);
     parse_igmp_message_type_field(ighdr->igmp_type);
 
-    printf("\n   |-Code      : %u\n", ighdr->igmp_code);
-    printf("   |-Checksum  : %u\n", ntohs(ighdr->igmp_cksum));
-    printf("   |-Group     : %u\n", inet_ntoa(ighdr->igmp_group));
+    printf("\n   |-Code      : %x\n", ighdr->igmp_code);
+    printf("   |-Checksum  : %x\n", ntohs(ighdr->igmp_cksum));
+    printf("   |-Group     : %x\n", inet_ntoa(ighdr->igmp_group));
 
-    printf("\n###########################################################");
+    printf("\n###########################################################\n");
 
 }
 
@@ -265,6 +276,7 @@ void print_ip_header(unsigned char* buffer, int size){
 }
 
 /* print ip6 header */
+
 void print_ip6_header(unsigned char* buffer, int size){
 
     char addrstr[INET6_ADDRSTRLEN];
@@ -276,12 +288,12 @@ void print_ip6_header(unsigned char* buffer, int size){
 
     printf("\nIPv6 Header\n");
 
-    printf("   |-Version         : %u\n", iphdr->version >> 4);
-    printf("   |-Traffic class   : %u\n", iphdr->traffic_class >> 20);
-    printf("   |-Flow label      : %u\n", ntohl(iphdr->flow_label & 0x000fffff));
-    printf("   |-Payload len     : %u\n", ntohs(iphdr->length));
-    printf("   |-Next header     : %u\n", iphdr->next_header);
-    printf("   |-Hop limit       : %u\n", iphdr->hop_limit);
+    printf("   |-Version         : %x\n", iphdr->version >> 4);
+    printf("   |-Traffic class   : %x\n", iphdr->traffic_class >> 20);
+    printf("   |-Flow label      : %x\n", ntohl(iphdr->flow_label & 0x000fffff));
+    printf("   |-Payload len     : %x\n", ntohs(iphdr->length));
+    printf("   |-Next header     : %x\n", iphdr->next_header);
+    printf("   |-Hop limit       : %x\n", iphdr->hop_limit);
 
     inet_ntop(AF_INET6, &iphdr->src, addrstr, sizeof(addrstr));
     printf("   |-Source IP      : %s\n", addrstr);
@@ -311,10 +323,10 @@ void print_tcp_packet(unsigned char* buffer, int size){
     print_ip_header(buffer,size);
         
     printf("\nTCP Header\n");
-    printf("   |-Source Port           : %u\n",ntohs(tcph->source));
-    printf("   |-Destination Port      : %u\n",ntohs(tcph->dest));
-    printf("   |-Sequence Number       : %u\n",ntohl(tcph->seq));
-    printf("   |-Acknowledge Number    : %u\n",ntohl(tcph->ack_seq));
+    printf("   |-Source Port           : %d\n",ntohs(tcph->source));
+    printf("   |-Destination Port      : %d\n",ntohs(tcph->dest));
+    printf("   |-Sequence Number       : %d\n",ntohl(tcph->seq));
+    printf("   |-Acknowledge Number    : %d\n",ntohl(tcph->ack_seq));
     printf("   |-Header Length         : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
     //printf("   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
     //printf("   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
@@ -340,7 +352,7 @@ void print_tcp_packet(unsigned char* buffer, int size){
     printf("Data Payload\n");    
     print_data(buffer+header_size, size-header_size);
                         
-    printf("\n###########################################################");
+    printf("\n###########################################################\n");
 }
 
 void print_dns_packet(unsigned char* buffer, int size){
@@ -363,14 +375,14 @@ void print_dns_packet(unsigned char* buffer, int size){
     else
         printf("   |-DNS response type\n");
 
-    printf("   |-Opcode      : %u\n",dndh->opcode);
-    printf("   |-R code : %u\n",dndh->rcode);
-    printf("   |-Q count    : %u\n",ntohs(dndh->q_count)); 
-    printf("   |-Answer    : %u\n",ntohs(dndh->ans_count));
-    printf("   |-Auth count    : %u\n",ntohs(dndh->auth_count));
-    printf("   |-Additional    : %u\n",ntohs(dndh->add_count));
+    printf("   |-Opcode         : %x\n",dndh->opcode);
+    printf("   |-R code         : %x\n",dndh->rcode);
+    printf("   |-Q count        : %x\n",ntohs(dndh->q_count)); 
+    printf("   |-Answer         : %x\n",ntohs(dndh->ans_count));
+    printf("   |-Auth count     : %x\n",ntohs(dndh->auth_count));
+    printf("   |-Additional     : %x\n",ntohs(dndh->add_count));
 
-    printf("\n###########################################################");
+    printf("\n###########################################################\n");
 }
 
 void print_udp_packet(unsigned char *buffer , int size){
@@ -397,7 +409,8 @@ void print_udp_packet(unsigned char *buffer , int size){
     printf("                        DATA Dump                         ");
     printf("\n");
 
-    if (ntohs(udph->source) == 53) print_dns_packet(buffer, size);
+    if (ntohs(udph->source) == 53) 
+        print_dns_packet(buffer, size);
     
     printf("\n");
     printf("IP Header\n");
@@ -411,7 +424,7 @@ void print_udp_packet(unsigned char *buffer , int size){
     //Move the pointer ahead and reduce the size of string
     print_data(buffer+header_size, size-header_size);
     
-    printf("\n###########################################################");
+    printf("\n###########################################################\n");
 }
 
 void print_icmp_packet(unsigned char* buffer , int size){
@@ -430,7 +443,7 @@ void print_icmp_packet(unsigned char* buffer , int size){
     print_ip_header(buffer , size);
         
     printf("\n\nICMP Header\n");
-    printf("   |-Type :          : %u\t", icmph->type);
+    printf("   |-Type :          : %x\t", icmph->type);
             
     if(icmph->type == 11){
         printf("(TTL Expired)\n");
@@ -439,12 +452,12 @@ void print_icmp_packet(unsigned char* buffer , int size){
         printf("(ICMP Echo Reply)\n");
     }
     
-    printf("   |-Code          : %u\n", icmph->code);
-    printf("   |-Checksum      : %u\n", ntohs(icmph->checksum));
-    printf("   |-ID            : %u\n", ntohs(icmph->un.echo.id));
-    printf("   |-Sequence      : %u\n", ntohs(icmph->un.echo.sequence));
+    printf("   |-Code          : %x\n", icmph->code);
+    printf("   |-Checksum      : %x\n", ntohs(icmph->checksum));
+    printf("   |-ID            : %x\n", ntohs(icmph->un.echo.id));
+    printf("   |-Sequence      : %x\n", ntohs(icmph->un.echo.sequence));
     printf("   |-Gateway       : %lu\n", icmph->un.gateway);
-    printf("   |-Mysterious    : %u\n", ntohs(icmph->un.frag.__unused));
+    printf("   |-Mysterious    : %x\n", ntohs(icmph->un.frag.__unused));
     printf("\n");
     printf("                        DATA Dump                         ");
     printf("\n");
@@ -459,7 +472,7 @@ void print_icmp_packet(unsigned char* buffer , int size){
     
     print_data(buffer + header_size, size-header_size);
     
-    printf("\n###########################################################");
+    printf("\n###########################################################\n");
 }
 
 /* print icmp6 packet */
@@ -470,14 +483,14 @@ void print_icmpv6_packet(unsigned char* buffer, int offset, int size){
     int header_size = offset + sizeof(icmp6);
 
     printf("\n\nICMPv6 Header\n");
-    printf("   |-Type            : %u\n", icmp6->type);
-    printf("   |-Code            : %u\n", icmp6->code);
-    printf("   |-Checksum        : %u\n", icmp6->cksum);
+    printf("   |-Type            : %x\n", icmp6->type);
+    printf("   |-Code            : %x\n", icmp6->code);
+    printf("   |-Checksum        : %x\n", icmp6->cksum);
 
     if ((icmp6->type == ICMP6_ECHO_REQUEST) || (icmp6->type == ICMP6_ECHO_REPLY)){
 
-        printf("   |-ICMPv6 ID      : %u\n", icmp6->data >> 16);
-        printf("   |-ICMPv6 Sequence : %u\n", icmp6->data & 0x0000ffff);
+        printf("   |-ICMPv6 ID      : %x\n", icmp6->data >> 16);
+        printf("   |-ICMPv6 Sequence : %x\n", icmp6->data & 0x0000ffff);
     }
 
     printf("\n");
@@ -495,6 +508,7 @@ void print_icmpv6_packet(unsigned char* buffer, int offset, int size){
     
     printf("\n###########################################################\n");
 }
+
 
 // nbr = max interfaces number in the list
 
@@ -524,7 +538,7 @@ int get_itf_list(char** itf_list, int nbr){
     freeifaddrs(ifaddr);
 
     return itf_nbr; 
-} 
+}
 
 // set up the socket in promiscuous too
 
@@ -569,6 +583,24 @@ int get_itf_index(int sock, const char* itf_name) {
         return -1;
     }
 
+    printf("\nDEBUG\t\t: successfully get the interface index\n\n");
+
+    int opt = 1;
+    
+    // another trick to set up interface in promiscuous mode
+    
+    if (setsockopt(sock, SOL_SOCKET, PACKET_MR_PROMISC,&opt, sizeof(opt)) < 0) {
+        printf("Server-setsockopt() error for PACKET_MR_PROMISC\n");
+        return -1;
+    }
+
+    static const int32_t sock_qdisc_bypass = 1;
+    int32_t sock_qdisc_ret = setsockopt(sock, SOL_PACKET, PACKET_QDISC_BYPASS, &sock_qdisc_bypass, sizeof(sock_qdisc_bypass));
+
+    if (sock_qdisc_ret == -1) {
+        perror("Can't enable QDISC bypass on socket\n");
+        return -1;
+    }
 
     if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
         perror("setsockopt error while adding SO_BINDTODEVICE\n");
@@ -577,47 +609,55 @@ int get_itf_index(int sock, const char* itf_name) {
 
     printf("\nDEBUG\t\t: setting setsockopt options DONE !\n\n");
 
-    assert(setup_promiscuous_mode(sock, ifr.ifr_ifindex) > 0);
+    //assert(setup_promiscuous_mode(sock, ifr.ifr_ifindex) > 0);
 
     return ifr.ifr_ifindex;
 }
-
 
 // last step : binding the socket to the device
 
 int bind_sock(int sock, int itf_index){
 
-    struct sockaddr_ll addr;
-    memset((void*)&addr, 0, sizeof(struct sockaddr_ll));
-    addr.sll_family = PF_PACKET;
-    addr.sll_protocol = htons(ETH_P_ALL);
-    addr.sll_ifindex = itf_index;
+    struct sockaddr_ll sock_addr;
+    memset((void*)&sock_addr, 0, sizeof(struct sockaddr_ll));
 
-    if (bind(sock,(struct sockaddr *)&addr, sizeof(struct sockaddr_ll)) < 0){
-        perror("error while binding the socket\n");
+    sock_addr.sll_family = AF_PACKET;
+    sock_addr.sll_protocol = htons(ETH_P_ALL);
+    sock_addr.sll_ifindex = itf_index;
+
+    if (bind(sock, (struct sockaddr *)&sock_addr, sizeof(struct sockaddr_ll)) < 0){
+        perror("fatal error while binding the socket\n");
         return -1;
     }
+
+    printf("\nDEBUG\t\t: socket ready to listen\n");
 
     return 0;
 }
 
-// creates and set up socket (setsockopt() etc...)
+// creates and set up socket (setsockopt() etc...) from an interface name to bind with
 
 int init_sock(const char *itf){
 
-    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    int sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
     if (sock == -1) {
         perror("error while creating socket\n");
         return -1;
     }
 
+    // getting interface index and setting it to god mode level
+
     int itf_index = get_itf_index(sock, itf);
-    assert(itf_index != -1);
 
-    printf("DEBUG : interface index : %d\n", itf_index);
+    if (itf_index < 0){
+        perror("fatal error occured while setting sock options\n");
+        return -1;
+    }
 
-    assert(bind_sock(sock, itf_index) == 0);
+    printf("DEBUG : interface index : %x\n", itf_index);
+
+    //assert(bind_sock(sock, itf_index) == 0);
 
     return sock;
 
@@ -625,8 +665,8 @@ int init_sock(const char *itf){
 
 // print raw data in ASCII and hex values 
 
-void print_data(unsigned char* data , int size)
-{
+void print_data(unsigned char* data , int size){
+
     int i , j;
     for(i=0 ; i < size ; i++)
     {
