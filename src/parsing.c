09e2df2ffdc8f69113d51d350584a064dba38c53
ARMANGAU Etienne <81378159+OzzyProjects@users.noreply.github.com>
2021-10-02 17:48:20 +0200

    Update parsing.c

--- src/parsing.c
+++ src/parsing.c
@@ -1,3 +1,5 @@
+/* Includes */
+
 #include "sock_utils.h"
 #include "parsing.h"
 
@@ -21,6 +23,91 @@ void parse_arp_opcode_field(uint8_t opcode_field){
 
 }
 
+void parse_pndcp_service_id_field(uint8_t service_id){
+
+	switch(service_id){
+
+		case PNDCP_SERVICE_ID_GET: 
+			printf("GET\n");
+			break;
+
+		case PNDCP_SERVICE_ID_SET: 
+			printf("SET\n");
+			break;
+
+		case PNDCP_SERVICE_ID_IDENTIFY: 
+			printf("IDENTIFY\n");
+			break;
+
+		case PNDCP_SERVICE_ID_HELLO: 
+			printf("HELLO\n");
+			break;
+
+		default:
+			printf("Unknown Value\n");
+
+	}
+}
+
+void parse_pndcp_service_type_field(uint8_t service_type){
+
+	switch(service_type){
+
+		case PNDCP_SERVICE_TYPE_REQUEST: 
+			printf("Request\n");
+			break;
+
+		case PNDCP_SERVICE_TYPE_RESPONSE_SUCCESS: 
+			printf("Response Succes\n");
+			break;
+
+		case PNDCP_SERVICE_TYPE_RESPONSE_UNSUPPORTED: 
+			printf("Response Unsupported\n");
+			break;
+
+		default:
+			printf("Unknown Value\n");
+
+	}
+}
+
+void parse_pndcp_option_field(uint8_t option){
+
+	switch(option){
+
+		case PNDCP_OPTION_IP: 
+			printf("IP\n");
+			break;
+
+		case PNDCP_OPTION_DEVICE: 
+			printf("Device\n");
+			break;
+
+		case PNDCP_OPTION_DHCP: 
+			printf("DHCP\n");
+			break;
+
+		case PNDCP_OPTION_RESERVED: 
+			printf("Reserved\n");
+			break;
+
+		case PNDCP_OPTION_CONTROL: 
+			printf("Control\n");
+			break;
+
+		case PNDCP_OPTION_DEVICEINITIATIVE: 
+			printf("Device initiative\n");
+			break;
+
+		case PNDCP_OPTION_ALL: 
+			printf("ALL\n");
+			break;
+
+		default:
+			printf("Manufacturer or Unknown\n");
+
+	}
+}
 
 // parse the LLTD field function with the most common values
 
