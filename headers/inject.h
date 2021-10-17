fe92d82d06242a414d74224418b73ac79d4f4344
ARMANGAU Etienne <81378159+OzzyProjects@users.noreply.github.com>
2021-10-17 06:39:37 +0200

    Update inject.h

--- headers/inject.h
+++ headers/inject.h
@@ -14,6 +14,10 @@
 #include <stddef.h>
 
 #include "sock_utils.h"
+#include "parsing.h"
+#include "string_utils.h"
+
+#define BROADCAST_ADDR (uint8_t[6]){0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
 
 // pseudo struct for icmp packet
 
@@ -25,6 +29,20 @@ typedef struct icmp_packet {
     u_char data[52];
 
 } icmp_packet;
+
+typedef struct arp_packet {
+
+    uint16_t hardware_type;
+    uint16_t protocol_type;
+    uint8_t hardware_len;
+    uint8_t protocol_len;
+    uint16_t opcode;
+    uint8_t sender_mac[MAC_LENGTH];
+    uint8_t sender_ip[IPV4_LENGTH];
+    uint8_t target_mac[MAC_LENGTH];
+    uint8_t target_ip[IPV4_LENGTH];
+
+} arp_packet;
 
 typedef struct opt_struct{
 
@@ -42,14 +60,15 @@ typedef struct opt_struct{
 //the thread function
 
 void *connection_handler(void*);
+unsigned char *get_target_mac_address(int, const char*);
 
 // perform ARP poisoning packets
 
-unsigned char* pack_arp_spoofing_packet(unsigned char*, unsigned char*, unsigned char*, unsigned char*,
-    unsigned char*);
-
 int get_device_index(int, const char*);
-int init_arp_sock(const char*, int*);
+struct ethhdr* create_arp_packet(const uint16_t, const uint8_t*, const char*, const uint8_t*, const char*);
+int send_broadcast_arp_packet(int, struct sockaddr_ll*, const uint8_t*, const char*, const char*);
+int start_arp_spoofing_attack(int, struct sockaddr_ll*, const uint8_t*, const char*, const uint8_t*, const char*, const uint8_t*);
+int launch_arp_spoofing_attack(const char*, const char*, const char*);
 int start_arp_poisonning(char*);
 
 // perform ICMP snooping here
